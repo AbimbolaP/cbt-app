561c08a97e23f4e1bfa7a8842863eed77ff1be61
AbimbolaP <abimbola.peace131@gmail.com>
2025-09-25 14:11:29 +0100

    prisma fixes

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -4,8 +4,25 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const questions = await prisma.question.findMany();
-  return NextResponse.json(questions);
+  // const questions = await prisma.question.findMany();
+  // return NextResponse.json(questions);
+  try {
+    const questions = await prisma.question.findMany();
+    return new Response(JSON.stringify(questions), {
+      status:200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching questions:", error);
+    return new Response(JSON.stringify({ error: "Failed to fetch questions"}), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
 }
 
 export async function POST(req: Request) {
